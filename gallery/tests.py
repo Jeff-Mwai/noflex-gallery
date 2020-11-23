e267f54ff700f523617bf894eb01c39cddf2151f
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-23 16:41:46 +0300

    Added test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category,Location,Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.location = Location(name='Cairo')
+        self.location.save_location()
+ 
+
+
+
+      
