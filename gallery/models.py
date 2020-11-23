b4f7fa35df156cd8872f6377ec9fe8a45e717ccc
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-23 16:42:16 +0300

    edited location

--- gallery/models.py
+++ gallery/models.py
@@ -16,26 +16,26 @@ class Category(models.Model):
         self.delete()
 
 
-class Location(models.Model):
-    name = models.CharField(max_length=60)
+# class Location(models.Model):
+#     name = models.CharField(max_length=60)
 
-    @classmethod
-    def get_locations(cls):
-        locations = Location.objects.all()
-        return locations
+#     @classmethod
+#     def get_locations(cls):
+#         locations = Location.objects.all()
+#         return locations
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    @classmethod
-    def update_location(cls, id, value):
-        cls.objects.filter(id=id).update(image=value)
+#     @classmethod
+#     def update_location(cls, id, value):
+#         cls.objects.filter(id=id).update(image=value)
 
-    def save_location(self):
-        self.save()
+#     def save_location(self):
+#         self.save()
 
-    def delete_location(self):
-        self.delete()
+#     def delete_location(self):
+#         self.delete()
 
 class Image(models.Model):
     image = CloudinaryField('image')
