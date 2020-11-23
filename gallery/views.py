59077d77a9ff827403d729f6f44d442fe1449844
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-23 16:43:37 +0300

    deployment to heroku

--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Image, Location, Category
 
-# Create your views here.
+
 def images(request):
     pictures = Image.objects.all()
     location = Location.objects.all()
